5d94df60d16888e8a77021e7113de7b1cc8db606
cmrmont <cmonterora@gmail.com>
2023-11-20 20:04:54 -0500

    tercer cambio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
+import { ShopLayout } from '../components/layouts'
 
-const inter = Inter({ subsets: ['latin'] })
+import { initialData } from '../public/products'
+
 
 export default function Home() {
   return (
-    <>
-      <Head>
-          <h1>Ecommerce Adso</h1>
-      </Head>
-      </>
-      )
-      }
-      
+    <ShopLayout titile={'Ecommerce Sena - Home'} pageDescription={'Encuentra los mejores articulos para tu setup'}>
+      <Typography variant='h1' component='h1'>Tienda</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>Articulos</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map(product => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component='img'
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
+    </ShopLayout>
+  )
+}
